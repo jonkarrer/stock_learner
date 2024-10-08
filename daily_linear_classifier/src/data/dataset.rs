9cce280991f828cce68ed36508b2dbd77f265328
jkarrer <jkarrer@protect247.com>
2024-10-08 09:57:54 -0400

    feat: created batcher

--- daily_linear_classifier/src/data/dataset.rs
+++ daily_linear_classifier/src/data/dataset.rs
@@ -1,4 +1,11 @@
-use burn::data::dataset::{Dataset, SqliteDataset};
+use burn::{
+    data::{
+        dataloader::batcher::Batcher,
+        dataset::{Dataset, SqliteDataset},
+    },
+    prelude::Backend,
+    tensor::{Int, Tensor},
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -36,6 +43,16 @@ pub struct DailyLinearDataset {
     dataset: SqliteDataset<DailyLinearItem>,
 }
 
+impl Dataset<DailyLinearItem> for DailyLinearDataset {
+    fn get(&self, index: usize) -> Option<DailyLinearItem> {
+        self.dataset.get(index)
+    }
+
+    fn len(&self) -> usize {
+        self.dataset.len()
+    }
+}
+
 impl DailyLinearDataset {
     pub fn train() -> Self {
         Self::new("train")
@@ -64,6 +81,106 @@ impl DailyLinearDataset {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct DailyLinearBatcher<B: Backend> {
+    device: B::Device,
+}
+
+#[derive(Clone, Debug)]
+pub struct DailyLinearBatch<B: Backend> {
+    pub inputs: Tensor<B, 2>,
+    pub targets: Tensor<B, 1>,
+}
+
+impl<B: Backend> DailyLinearBatcher<B> {
+    pub fn new(device: B::Device) -> Self {
+        Self { device }
+    }
+
+    pub fn min_max_norm<const D: usize>(&self, inp: Tensor<B, D>) -> Tensor<B, D> {
+        let min = inp.clone().min_dim(0);
+        let max = inp.clone().max_dim(0);
+        (inp.clone() - min.clone()).div(max - min)
+    }
+}
+
+impl<B: Backend> Batcher<DailyLinearItem, DailyLinearBatch<B>> for DailyLinearBatcher<B> {
+    fn batch(&self, items: Vec<DailyLinearItem>) -> DailyLinearBatch<B> {
+        let mut inputs: Vec<Tensor<B, 2>> = Vec::new();
+
+        for item in items.iter() {
+            let input_tensor = Tensor::<B, 1>::from_floats(
+                [
+                    item.open_price,
+                    item.close_price,
+                    item.high_price,
+                    item.low_price,
+                    item.volume,
+                    item.volume_weighted_price,
+                    item.bar_trend as f32,
+                    item.hundred_day_sma,
+                    item.hundred_day_ema,
+                    item.fifty_day_sma,
+                    item.fifty_day_ema,
+                    item.twenty_day_sma,
+                    item.twenty_day_ema,
+                    item.nine_day_sma,
+                    item.nine_day_ema,
+                    item.hundred_day_high,
+                    item.hundred_day_low,
+                    item.fifty_day_high,
+                    item.fifty_day_low,
+                    item.ten_day_high,
+                    item.ten_day_low,
+                    item.fourteen_day_rsi,
+                    item.top_bollinger_band,
+                    item.middle_bollinger_band,
+                    item.bottom_bollinger_band,
+                ],
+                &self.device,
+            );
+
+            // make the tensor 2D for easy concat later
+            // inputs = [
+            //     Tensor([[10.5, 100.0, 50.2]]),
+            //     Tensor([[20.1, 95.5, 60.0]]),
+            //     Tensor([[15.3, 105.2, 55.7]]),
+            // ];
+            inputs.push(input_tensor.unsqueeze());
+        }
+
+        // concat the tensors, now the shape is (batch_size, feature length)
+        // inputs = Tensor([
+        //     [10.5, 100.0, 50.2],
+        //     [20.1, 95.5, 60.0],
+        //     [15.3, 105.2, 55.7]
+        // ])
+        let inputs = Tensor::cat(inputs, 0);
+
+        // normalize the inputs so that they fit between 0 and 1
+        let inputs = self.min_max_norm(inputs);
+
+        // create target tenser
+        // targets = [
+        //     Tensor([1.0]),
+        //     Tensor([0.0]),
+        //     Tensor([1.0]),
+        //     Tensor([1.0]),
+        //     Tensor([0.0])
+        // ]
+        let targets = items
+            .iter()
+            .map(|item| Tensor::<B, 1>::from_floats([item.label as f32], &self.device))
+            .collect();
+
+        // do not need to unsqueeze here, just concat for a 1D tensor
+        // targets = Tensor([1.0, 0.0, 1.0, 1.0, 0.0])
+        let targets = Tensor::cat(targets, 0);
+
+        DailyLinearBatch { inputs, targets }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -71,9 +188,9 @@ mod tests {
     #[test]
     fn test_dataset() {
         let train = DailyLinearDataset::train();
-        // let valid = DailyLinearDataset::valid();
+        let valid = DailyLinearDataset::valid();
 
-        dbg!(train.dataset.len());
-        assert!(false);
+        assert_eq!(train.dataset.len(), 759839);
+        assert_eq!(valid.dataset.len(), 219260);
     }
 }
